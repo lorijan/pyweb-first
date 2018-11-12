7c45c2ca5ad45fbd725e855d2cf3ebb2eaffac35
Lorincz Janos <lorijan@reak.bme.hu>
2018-11-12 21:50:57 +0100

    Changes to be committed: 	modified:   myapp/__init__.py     new file:   myapp/auth.py 	new file:   myapp/blog.py 	new file:   myapp/static/style.css 	new file:   myapp/templates/auth/login.html 	new file:   myapp/templates/auth/register.html 	new file:   myapp/templates/base.html 	new file:   myapp/templates/blog/create.html 	new file:   myapp/templates/blog/index.html 	new file:   myapp/templates/blog/update.html

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -45,7 +45,23 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+###############################################################################
     from . import db
     db.init_app(app)
+
+###############################################################################
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+###############################################################################
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    '''
+    Unlike the auth blueprint, the blog blueprint does not have a url_prefix. So the index view will be at /, the create view at /create, and so on. The blog is the main feature of Flaskr, so it makes sense that the blog index will be the main indexself.
+    However, the endpoint for the index view defined below will be blog.index. Some of the authentication views referred to a plain index endpoint. app.add_url_rule() associates the endpoint name 'index' with the / url so that url_for('index') or url_for('blog.index') will both work, generating the same / URL either way.
+
+    In another application you might give the blog blueprint a url_prefix and define a separate index view in the application factory, similar to the hello view. Then the index and blog.index endpoints and URLs would be different.
+    '''
 
     return app
